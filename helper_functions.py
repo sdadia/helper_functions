940a152fa7bd7f575bde05f30c580eec3aade2f4
Sahil Dadia <dadiasahil94@yahoo.in>
2016-09-07 14:05:38 +0530

    Modidfied everything

--- helper_functions.py
+++ helper_functions.py
@@ -5,12 +5,18 @@ import matplotlib.pyplot as plt
 
 def PLOT_IMAGE(image, figure_num=1, show=False):
     '''
-    Parameters:
-        image = any image
-        figure_num *= figure_num number
+    Parameters
+    ------------
+    image : Numpy array
+        Image to be shown
+    figure_num : (Optional) int
+        Figure number
+    show : (Optional) bool
+        If true shows O/P immediately, else you need to type plt.show()
 
-    Returns:
-        None
+    Returns
+    ------------
+    None
     '''
     plt.figure(figure_num)
     plt.imshow(image)
@@ -20,21 +26,98 @@ def PLOT_IMAGE(image, figure_num=1, show=False):
     return None
 
 
-def GINPUT_ROUTINE(image, num_pts=2):
+def GINPUT_ROUTINE(image, num_pts=-1):
     '''
-    Get coordinates of points in image
+    Get coordinates of points in image, click middle click to end
+    If num_pts == -1, then choose points indefinately until middle click of mouse
 
-    Parameters:
+    Parameters
+    ------------
     image : Numpy array
         Image where points are to be selected
 
-    Returns:
+    Returns
+    ------------
     coordinates : Numpy array
         Numpy array containing the coordinates of the points (row,col) format
     '''
     PLOT_IMAGE(image, show=False)
-    print("Please select" + str(num_pts) + "points")    # timeout : time(sec) to wait until termination, if input not given
-    coordinates = np.array(plt.ginput(n=num_pts, timeout=10))
+    print("Please select" + str(num_pts) + "points")
+    # timeout : time(sec) to wait until termination, if input not given
+    coordinates = plt.ginput(n=num_pts, timeout=0)
+    coordinates = np.array(coordinates)
     coordinates[:, 0], coordinates[:, 1] = coordinates[:, 1], coordinates[:, 0].copy() # Exchange col1 and col2 to get (row_coordinate, col_coordinate)
 
     return coordinates.astype(np.uint8) # Return +ve integers only
+
+
+def RESIZE_IMAGE(image1, fx1, fy1):
+    '''
+    Function to resize the image
+
+    Parameters:
+    ------------
+    image1 : Numpy array
+        Image to be resized
+    fx1 : float
+        Horizontal stretch (>0.1)
+    fy1 : float
+        Vertical stretch (>0.1)
+
+    Returns
+    ------------
+    Resized image
+    '''
+    if(image1 != None):
+        return cv2.resize(image1, (0, 0), fx=fx1, fy=fy1)
+    else:
+        print("Image incorrect/Image is NONE")
+
+
+def PLOT_IMAGE_CV(img,  wait_time=0, window_name="name"):
+    '''
+    Show's image on OpenCV format
+
+    Parameters
+    ------------
+    img : Numpy array
+        Image to show
+    window_name : (Optional) String
+        Window name
+    wait_time : (Optional) int
+        Wait time before closng the window automatically
+
+    Returns
+    ------------
+    None
+    '''
+    cv2.imshow(window_name, img)
+    cv2.waitKey(wait_time) and 0xFF
+
+
+def PLOT_COLOR_HISTOGRAM(img, show = True, color = ('b','g','r')):
+    '''
+    Plot color histogram of image
+
+    Parameters
+    ------------
+    img : Numpy array
+        Image whose histogram is to be calculated
+    show : (Optional) bool
+        If true shows O/P immediately, else you need to type plt.show()
+    color : (Optional) Tuple of strings
+        Colors to be used
+
+    Returns
+    ------------
+    None
+    '''
+    color = ('b','g','r')
+    for i,col in enumerate(color):
+        histr = cv2.calcHist([img],[i],None,[256],[0,256])
+        plt.plot(histr,color = col)
+        max_pixel_intensity = np.max(np.max(img[:,:,0]), np.max(img[:,:,1]), np.max(img[:,:,2]))
+        min_pixel_intensity = np.min(np.min(img[:,:,0]), np.min(img[:,:,1]), np.min(img[:,:,2]))
+        plt.xlim([min_pixel_intensity, max_pixel_intensity])
+    if (show is True):
+        plt.show()

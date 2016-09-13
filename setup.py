5902f83f201a0bc2bdeb2e9378071549ec422d1e
Sahil Dadia <dadiasahil94@yahoo.in>
2016-09-13 16:06:05 +0530

    Updated setup.py for version 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
         name            =  'helper_functions',
-        version         =  '1.0.3',
+        version         =  '1.0.0',
         py_modules      =  ['helper_functions'],
         author          =  'Sahil Dadia',
         author_email    =  'dadiasahil94@yahoo.in',

69fcf2992b1aa7de5f2815bd13664786f511cc1a
Sahil Dadia <dadiasahil94@yahoo.in>
2016-09-13 14:38:03 +0530

    Modief setup files and fixed bugs in GINPUT function

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 
 setup(
         name            =  'helper_functions',
-        version         =  '1.0.0',
+        version         =  '1.0.3',
         py_modules      =  ['helper_functions'],
         author          =  'Sahil Dadia',
         author_email    =  'dadiasahil94@yahoo.in',
         url             = 'https://github.com/sdadia/helper_functions.git',
         description     = 'A simple module of simple function, for opencv and python3',
-        license         = 'LICENSE'
+        license         = 'MIT-LICENSE'
     )

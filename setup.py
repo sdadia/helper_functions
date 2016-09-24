c03b5d25fd13d13ebdc982a8d5ad51109bbbb1f9
Sahil Dadia <dadiasahil94@yahoo.in>
2016-09-24 15:30:53 +0530

    Started over

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
 from distutils.core import setup
 
 setup(
-        name            =  'helper_functions',
-        version         =  '1.0.0',
-        py_modules      =  ['helper_functions'],
-        author          =  'Sahil Dadia',
-        author_email    =  'dadiasahil94@yahoo.in',
-        url             = 'https://github.com/sdadia/helper_functions.git',
-        description     = 'A simple module of simple function, for opencv and python3',
-        license         = 'MIT-LICENSE'
+        name            =   'helper_functions',
+        # packages        =   ['helper_functions'], # this must be the same as the name above
+        version         =   '2.0.0',
+        py_modules      =   ['helper_functions'],
+        author          =   'Sahil Dadia',
+        author_email    =   'dadiasahil94@yahoo.in',
+        url             =   'https://github.com/sdadia/helper_functions.git',# use the URL to the github repo
+        description     =   'A simple module of simple function, for opencv and python3',
+        license         =   'LICENSE.txt',
+        keywords        =   ['opencv', 'helper', 'scripts'], # arbitrary keywords
+        classifiers     =   [
+                            'Topic :: Utilities',
+                            'Development Status :: 3 - Alpha',
+                            'Intended Audience :: Developers',
+                            'License :: OSI Approved :: MIT License',
+                            'Operating System :: OS Independent',
+                            'Programming Language :: Python :: 2',
+                            'Natural Language :: English'
+                            ]
     )
